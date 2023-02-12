57419bbcc0535cef2a4994fb4d0c13e4c494fb79
Supriya Chavan <supriyasonvane2@gmail.com>
2023-02-11 18:29:58 -0800

    Added Requirements.txt and model and route files

---
+++ __init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
+
+db = SQLAlchemy()
+migrate = Migrate()
+load_dotenv()
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/supriya/AC2/capstones/venv"
+set -gx VIRTUAL_ENV "/Users/supriya/Backend-tennis-score-keeping/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
