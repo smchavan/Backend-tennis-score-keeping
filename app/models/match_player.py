fafa6efd0f6c0b5d23d8085dddb1e486b2b49cca
Supriya Chavan <supriyasonvane2@gmail.com>
2023-02-14 08:24:35 -0800

    Modified Match_player for unique ID with two foreign keys

--- app/models/match_player.py
+++ app/models/match_player.py
@@ -2,17 +2,17 @@ from app import db
 import datetime
 
 class Match_player(db.Model):
-    id = db.Column(db.Integer, primary_key=True,nullable=False)
+    #id = db.Column(db.Integer, primary_key=True,nullable=False)
 
-    match_id = db.Column(db.Integer, db.ForeignKey('match.id'),nullable=False)
-    player_id = db.Column(db.Integer, db.ForeignKey('player.id'),nullable=False)
+    match_id = db.Column(db.Integer, db.ForeignKey('match.id'),primary_key=True,  nullable=False)
+    player_id = db.Column(db.Integer, db.ForeignKey('player.id'),primary_key=True,  nullable=False)
 
     matches = db.relationship("Match", back_populates="match_players")
     players = db.relationship("Player", back_populates="match_players")
 
     def to_dict(self):
         match_player_dict = {}
-        match_player_dict["id"] = self.id
+        #match_player_dict["id"] = self.id
         match_player_dict["match_id"] = self.match_id
         match_player_dict["player_id"] = self.player_id
         
--- app/routes/match_routes.py
+++ app/routes/match_routes.py
@@ -8,7 +8,23 @@ from app.models.game import Game
 from app.models.stat import Stat
 from app.routes.routes_helper import validate_model
 
-matchs_bp = Blueprint("matchs_bp", __name__, url_prefix="/matchs")
+matchs_bp = Blueprint("matchs_bp", __name__, url_prefix="/matches")
 
 # ## `POST /matchs`
 # #@matchs_bp.route("/matchs", methods=["POST"])
+
+# Create a new match - POST
+@matchs_bp.route("/match", methods=["POST"])
+def create_match():
+    request_body = request.get_json()
+    new_match = Match(
+            no_of_sets=request_body["no_of_sets"],
+            no_of_gamesperset=request_body["no_of_gamesperset"],
+            match_date=request_body["match_date"],
+            match_name=request_body["match_name"],
+            player_a_id=request_body["player_a_id"],
+            player_b_id=request_body["player_b_id"],
+            user_id=request_body["user_id"]
+        )
+    db.session.add(new_match)
+    db.session.commit()
