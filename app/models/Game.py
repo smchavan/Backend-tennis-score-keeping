f1234c42973c21d6e9fb7977c1a15f0b7f48d855
Supriya Chavan <supriyasonvane2@gmail.com>
2023-02-15 14:43:33 -0800

    Added Game Routes All five and tested

--- app/models/Game.py
+++ app/models/Game.py
@@ -3,8 +3,8 @@ from app import db
 class Game(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     game_number = db.Column(db.Integer) #Todo- add autoincrement
-    player_a_score = db.Column(db.Integer)
-    player_b_score = db.Column(db.Integer)
+    player_a_score = db.Column(db.Integer,default=0)
+    player_b_score = db.Column(db.Integer,default=0)
     set_id = db.Column(db.Integer, db.ForeignKey("set.id"), nullable=False)
     
     set = db.relationship("Set",back_populates="games")
@@ -15,16 +15,20 @@ class Game(db.Model):
         game_dict["game_number"] = self.game_number
         game_dict["player_a_score"] = self.player_a_score
         game_dict["player_b_score"] = self.player_b_score
+        game_dict["set_id"] = self.set_id
         
-        return 
+        return game_dict
 
     @classmethod
     def from_dict(cls, game_data):
         new_game = Game(game_number=game_data["game_number"],
-                    player_a_game_won=game_data["player_a_games_won"],
-                    player_b_games_won=game_data["player_b_games_won"],                    
-                    registered_at = game_data["registered_at"],
-                    player_names=game_data["player_names"],
-                    games_ids=game_data["games_ids"]
+                    player_a_score=game_data["player_a_score"],
+                    player_b_score=game_data["player_b_score"],                    
+                    set_id=game_data["set_id"]
                     )
         return new_game
+    
+
+
+
+
